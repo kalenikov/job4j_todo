0d50b08d9c9662c5abefb940f9a4ff35b3ba2d6f
serj <sergeykalenikov@gmail.com>
2021-09-02 16:57:42 +0300

    3. Лямбды и шаблон wrapper. [#49295]

--- src/main/java/ru/job4j/todo/HibernateUtil.java
+++ src/main/java/ru/job4j/todo/HibernateUtil.java
@@ -1,9 +1,14 @@
 package ru.job4j.todo;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class HibernateUtil {
     private static SessionFactory sessionFactory;
@@ -20,5 +25,34 @@ public class HibernateUtil {
                     .buildMetadata().buildSessionFactory();
         }
         return sessionFactory;
+    }
+
+    public static <T> T doInTransactionWithReturn(Function<Session, T> command) {
+        Session session = getSessionFactory().openSession();
+        Transaction tx = session.beginTransaction();
+        try {
+            T rsl = command.apply(session);
+            tx.commit();
+            return rsl;
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+            throw e;
+        } finally {
+            session.close();
+        }
+    }
+
+    public static void doInTransaction(Consumer<Session> consumer) {
+        Session session = getSessionFactory().openSession();
+        Transaction tx = session.beginTransaction();
+        try {
+            consumer.accept(session);
+            tx.commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+            throw e;
+        } finally {
+            session.close();
+        }
     }
 }
--- src/main/java/ru/job4j/todo/repository/HibernateItemRepository.java
+++ src/main/java/ru/job4j/todo/repository/HibernateItemRepository.java
@@ -1,11 +1,11 @@
 package ru.job4j.todo.repository;
 
-import org.hibernate.Session;
-import ru.job4j.todo.HibernateUtil;
 import ru.job4j.todo.model.Item;
 
-import java.sql.SQLException;
 import java.util.List;
+
+import static ru.job4j.todo.HibernateUtil.doInTransaction;
+import static ru.job4j.todo.HibernateUtil.doInTransactionWithReturn;
 
 public class HibernateItemRepository implements ItemRepository {
 
@@ -17,34 +17,19 @@ public class HibernateItemRepository implements ItemRepository {
         return SingletonHolder.INSTANCE;
     }
 
-
     @Override
     public Item save(Item item) {
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        session.beginTransaction();
-        session.saveOrUpdate(item);
-        session.getTransaction().commit();
-        session.close();
+        doInTransaction(session -> session.saveOrUpdate(item));
         return item;
     }
 
     @Override
     public List<Item> getAll() {
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        session.beginTransaction();
-        List<Item> result = session.createQuery("from Item order by id", Item.class).getResultList();
-        session.getTransaction().commit();
-        session.close();
-        return result;
+        return doInTransactionWithReturn(session -> session.createQuery("from Item order by id", Item.class).getResultList());
     }
 
     @Override
     public Item get(Integer id) {
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        session.beginTransaction();
-        Item item = session.find(Item.class, id);
-        session.getTransaction().commit();
-        session.close();
-        return item;
+        return doInTransactionWithReturn(session -> session.find(Item.class, id));
     }
 }
